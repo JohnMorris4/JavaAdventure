44c9dc95496008304c129a9edd200f02cff634a5
John Morris <johnmorris4@bellsouth.net>
2018-01-19 07:18:34 -0500

    Finished Initial setup and basic Functions withinn the Game...

--- src/com/morrisje/Main.java
+++ src/com/morrisje/Main.java
@@ -19,16 +19,55 @@ public class Main {
         locations.put(5, new Location(5, "You are beside the mountain"));
         locations.put(6, new Location(6, "You are in the forest"));
 
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("W", 6);
+        //locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 5);
+//        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+//        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+//        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+//        locations.get(5).addExit("Q", 0);
+
+
+//        locations.get(6).addExit("Q", 0);
+
         int loc = 1;
-        while (true){
-            System.out.println(locations.get(loc).getDescription());
-            if (loc == 0){
-                break;
-            }
-            loc = scanner.nextInt();
-            if(!locations.containsKey(loc)){
-                System.out.println("You are trying to move to an invalid location");
-            }
-        }
+//        while (true){
+//            System.out.println(locations.get(loc).getDescription());
+//            if (loc == 0){
+//                break;
+//            }
+//
+//            Map<String, Integer> exits = locations.get(loc).getExits();
+//            System.out.print("Available Exits Are: " );
+//            for(String exit: exits.keySet()) {
+//                System.out.print(exit + ", " );
+//            }
+//            System.out.println();
+//
+//            String direction = scanner.nextLine().toUpperCase();
+//
+//            if(exits.containsKey(direction)) {
+//            loc = exits.get(direction);
+//            } else {
+//                System.out.println("You cant go in that direction");
+//            }
+//
+//            if(!locations.containsKey(loc)){
+//                System.out.println("You are trying to move to an invalid location");
+//            }
+//        }
     }
 }
