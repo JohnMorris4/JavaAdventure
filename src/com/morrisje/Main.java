afa75622d33cd670bace191ac4e451417a307946
John Morris <johnmorris4@bellsouth.net>
2018-01-21 05:06:52 -0500

    Finished up the Immutiable Classes

--- src/com/morrisje/Main.java
+++ src/com/morrisje/Main.java
@@ -11,63 +11,81 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "You are sitting in front of the computer learning Java"));
-        locations.put(1, new Location(1, "You are beside a brook near the forest"));
-        locations.put(2, new Location(2, "You are awaiting for the mail for your Java books"));
-        locations.put(3, new Location(3, "You are on top of a hill"));
-        locations.put(4, new Location(4, "You are sitting in a chair watching a movie"));
-        locations.put(5, new Location(5, "You are beside the mountain"));
-        locations.put(6, new Location(6, "You are in the forest"));
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+        locations.put(0, new Location(0, "You are sitting in front of the computer learning Java", tempExit));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
-        locations.get(1).addExit("W", 6);
-        //locations.get(1).addExit("Q", 0);
-
-        locations.get(2).addExit("N", 5);
-//        locations.get(2).addExit("Q", 0);
-
-        locations.get(3).addExit("W", 1);
-//        locations.get(3).addExit("Q", 0);
-
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-//        locations.get(4).addExit("Q", 0);
-
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
-//        locations.get(5).addExit("Q", 0);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("E", 3);
+        tempExit.put("S", 4);
+        tempExit.put("N", 5);
+        locations.put(1, new Location(1, "You are beside a brook near the forest", tempExit));
 
 
-//        locations.get(6).addExit("Q", 0);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 5);
+        locations.put(2, new Location(2, "You are awaiting for the mail for your Java books", tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 1);
+        locations.put(3, new Location(3, "You are on top of a hill", tempExit));
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N", 1);
+        tempExit.put("W", 2);
+        locations.put(4, new Location(4, "You are sitting in a chair watching a movie", tempExit));
+
+
+
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("S", 1);
+        tempExit.put("W", 2);
+        locations.put(5, new Location(5, "You are beside the mountain", tempExit));
+
+        Map<String, String> vocabulary = new HashMap<String, String>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("WEST", "W");
+
+
+
 
         int loc = 1;
-//        while (true){
-//            System.out.println(locations.get(loc).getDescription());
-//            if (loc == 0){
-//                break;
-//            }
-//
-//            Map<String, Integer> exits = locations.get(loc).getExits();
-//            System.out.print("Available Exits Are: " );
-//            for(String exit: exits.keySet()) {
-//                System.out.print(exit + ", " );
-//            }
-//            System.out.println();
-//
-//            String direction = scanner.nextLine().toUpperCase();
-//
-//            if(exits.containsKey(direction)) {
-//            loc = exits.get(direction);
-//            } else {
-//                System.out.println("You cant go in that direction");
-//            }
-//
-//            if(!locations.containsKey(loc)){
-//                System.out.println("You are trying to move to an invalid location");
-//            }
-//        }
+        while (true){
+            System.out.println(locations.get(loc).getDescription());
+            if (loc == 0){
+                break;
+            }
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available Exits Are: " );
+            for(String exit: exits.keySet()) {
+                System.out.print(exit + ", " );
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word: words){
+                    if(vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
+            if(exits.containsKey(direction)) {
+            loc = exits.get(direction);
+            } else {
+                System.out.println("You cant go in that direction");
+            }
+
+            if(!locations.containsKey(loc)){
+                System.out.println("You are trying to move to an invalid location");
+            }
+        }
     }
 }
